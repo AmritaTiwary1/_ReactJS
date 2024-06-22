c51fce655cafd0d5cae7b52f8d635250fa9cb0d5
AmritaTiwary1 <amritatiwary112@gmail.com>
2024-06-22 21:09:51 +0530

    useState is easy!

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,6 +1,5 @@
-
-
-
+/*
+// CHANGING THE VARIABLE VALUE THROUGH FUNCTION IS NOT WORKING PROPERLY , WITHOUT USING HOOKS
 function App() {
   let counter = 5;
   const add = () => {
@@ -23,5 +22,41 @@ function App() {
  </>
  )
 }
-
 export default App
+*/
+
+//NOW , USING HOOKS(READ GITHUB REPO ON REACT TO KNOW MORE ABOUT HOOKS)
+
+import { useState } from "react";
+//without importing react , it will work properly, bcoz bable will handle all this internally
+
+function App(){
+ // let counter = 5; //no need to declare variable, this will be handled through usestate syntax
+  
+//const [counter,setCounter]=useState(1); //if this is starting with const , then except setCounter fn , no one can change its value , eg=> counter-- ; setCounter(counter) === ERROR
+let [counter,setCounter]=useState(1);
+
+  
+  const add = () => {
+    console.log(counter) 
+    setCounter(counter+1)
+  }
+  function remove(){
+    console.log(counter); 
+    counter--;
+    setCounter(counter)
+    
+  }
+  return(
+ <>
+ <h1>Reactjs Deep Dive</h1>
+ <h2>Counter Value : {counter}</h2>
+
+ <button onClick={add} >ADD VALUE {counter}</button>
+ <br />
+ <br />
+ <button onClick={remove} >REMOVE BUTTON {counter}</button>
+ </>
+ )
+}
+export default App 
