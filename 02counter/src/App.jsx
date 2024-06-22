caf41fe9644ddb0717b955109a82e177a73db714
AmritaTiwary1 <amritatiwary112@gmail.com>
2024-06-22 20:29:50 +0530

    creation of new folder and changing variable value through function

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,27 @@
+
+
+
+function App() {
+  let counter = 5;
+  const add = () => {
+    console.log(counter) //this will print +1 value of previous counter in console but counter will not be updated on DOM 
+    counter = counter+1 //no change in DOM but +1 is happening in console
+  }
+  function remove(){
+    console.log(counter); ////this will print -1 value of previous counter in console but counter will not be updated on DOM 
+    counter--
+  }
+  return(
+ <>
+ <h1>Reactjs Deep Dive</h1>
+ <h2>Counter Value : {counter}</h2>
+
+ <button onClick={add} >ADD VALUE </button>
+ <br />
+ <br />
+ <button onClick={remove} >REMOVE BUTTON</button>
+ </>
+ )
+}
+
+export default App
