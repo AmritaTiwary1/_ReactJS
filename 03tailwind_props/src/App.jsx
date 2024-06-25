757875c03f86fb4b13e9fb133305e872a58d0d4d
AmritaTiwary1 <amritatiwary112@gmail.com>
2024-06-25 22:41:33 +0530

    installed tailwind in vite

---
+++ 03tailwind_props/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <h1 className='bg-blue-400 m-45'>Tailwind Test</h1>
+   
+    </>
+  )
+}
+
+export default App
---
+++ 03tailwind_props/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [   //I change this bcoz i installed tailwind 
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
