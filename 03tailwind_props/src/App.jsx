8018d1986223034a781e79923d57dae8cb608d1b
AmritaTiwary1 <amritatiwary112@gmail.com>
2024-06-26 13:35:20 +0530

    using props keyword in card.jsx

--- 03tailwind_props/src/App.jsx
+++ 03tailwind_props/src/App.jsx
@@ -2,16 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card' 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ let myobj={
+  username:"dev",
+  age:21
+ }
+ let arr=[1,2,3]
   return (
     <>
    <h1 className='bg-blue-400 m-45'>Tailwind Test</h1>
-   
+<Card channel="Amrita" btnText="TEXT WRITTEN BY ME" ></Card>  {/*values which we pass here will be shown as props in card.jsx  */}
+
+{/* <Card channel="Amrita" myArr=[1,2,3] ></Card>  ERROR :here is error {myArr=[1,2,3]} [plugin:vite:react-babel] C:\Users\Aman\Desktop\ReactJS\03tailwind_props\src\App.jsx: JSX value should be either an expression or a quoted JSX text. <Card channel="Amrita" myArr=[1,2,3] ></Card> (13:29) */}
+
+ <Card myarr={arr}></Card>  {/*WE CAN PASS ARRAY AS VARIABLE */}
+
+ <Card channel="DEV" someObj={{name:"dev"}} ></Card> {/* NO ERROR */}
+
+ {/* <Card channel="Amrita" obj={name:"dev"} ></Card> ERROR: [plugin:vite:react-babel] C:\Users\Aman\Desktop\ReactJS\03tailwind_props\src\App.jsx: Unexpected token, expected "}" (17:33) */}
+
+{/* <Card channel="Amrita" someObj=myobj ></Card>   ERROR */}
+
+<Card channel="RADHA" someObj={myobj}></Card> {/*WORKING FINE , MEANS WE CAN PASS OBJ AS VARIABLE */}
+
     </>
   )
-}
+} 
 
 export default App
---
+++ 03tailwind_props/src/components/Card.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+
+// i } USING PROPS KEYWORD
+// ii } WITHOUT USING PROPS KEYWORD ,DIRECTLY USING IT AS VARIABLE OF FUNCTION {propsNAME}
+
+
+//**************************************************************************************************************** */
+
+
+// i } USING PROPS KEYWORD
+export default function Card(props) {
+  
+  //console.log(props); //props is a defined keyword in react internally , and it is in form of object [collection of items]
+ 
+  console.log(props.myarr);  //(3) [1, 2, 3]  ---> only one times, bcoz i had written arr in one card only
+ 
+  console.log(props.channel);  //4 Amrita === 4times amrita bcoz i had written 4 times <card channel="amrita"...
+  return (
+    <>
+     <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-80 h-100">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+   src="https://images.pexels.com/photos/7319105/pexels-photo-7319105.jpeg" alt="" width="384" height="512"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure>
+
+    </>
+  )
+}
+
+
+
+// ii } WITHOUT USING PROPS KEYWORD ,DIRECTLY USING IT AS VARIABLE OF FUNCTION {propsNAME}
+
+// export default function Card({channel,btnText}) {  //NOTE : using bracket --- {propsName1,propsName2...}
+  
+ 
+//   return (
+//     <>
+//      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-80 h-100">
+//   <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+//    src="https://images.pexels.com/photos/7319105/pexels-photo-7319105.jpeg" alt="" width="384" height="512"/>
+//   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+//     <blockquote>
+//       <p className="text-lg font-medium">
+//         “Tailwind CSS is the only framework that I've seen scale
+//         on large teams. It’s easy to customize, adapts to any design,
+//         and the build size is tiny.”
+//       </p>
+//     </blockquote>
+//     <figcaption className="font-medium">
+//       <div className="text-sky-500 dark:text-sky-400">{channel}
+//       </div>
+//       <div className="text-slate-700 dark:text-slate-500">
+//          {btnText || "App.jsx didnt pass btnText as props in this card"}       {/* if no value is passed in btnText in card as props then (btnText || "this value will be taken in the place of btnText")  */}
+//       </div>
+//     </figcaption>
+//   </div>
+// </figure>
+
+//     </>
+//   )
+// }
+
