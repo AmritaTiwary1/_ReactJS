f990e69c87d4c31d06219782a433b9c086bc1b58
AmritaTiwary1 <amritatiwary112@gmail.com>
2024-06-26 13:44:01 +0530

    default value of props incase prop isnt passed in some card

--- 03tailwind_props/src/components/Card.jsx
+++ 03tailwind_props/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 // i } USING PROPS KEYWORD
 // ii } WITHOUT USING PROPS KEYWORD ,DIRECTLY USING IT AS VARIABLE OF FUNCTION {propsNAME}
-
+// iii } WHEN PROPS VALUE ARE NOT PASSED IN SOME CARD IN APP.JSX
 
 //**************************************************************************************************************** */
 
@@ -44,10 +44,44 @@ export default function Card(props) {
 }
 */
 
+//****************************************************************************************************************** */
 
 // ii } WITHOUT USING PROPS KEYWORD ,DIRECTLY USING IT AS VARIABLE OF FUNCTION {propsNAME}
 
-export default function Card({channel,btnText}) {  //NOTE : using bracket --- {propsName1,propsName2...}
+// export default function Card({channel,btnText}) {  //NOTE : using bracket --- {propsName1,propsName2...}
+  
+//   return (
+//     <>
+//      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-80 h-100">
+//   <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+//    src="https://images.pexels.com/photos/7319105/pexels-photo-7319105.jpeg" alt="" width="384" height="512"/>
+//   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+//     <blockquote>
+//       <p className="text-lg font-medium">
+//         “Tailwind CSS is the only framework that I've seen scale
+//         on large teams. It’s easy to customize, adapts to any design,
+//         and the build size is tiny.”
+//       </p>
+//     </blockquote>
+//     <figcaption className="font-medium">
+//       <div className="text-sky-500 dark:text-sky-400">{channel}
+//       </div>
+//       <div className="text-slate-700 dark:text-slate-500">
+//          {btnText || "App.jsx didnt pass btnText as props in this card"}       {/* if no value is passed in btnText in card as props then (btnText || "this value will be taken in the place of btnText")  */}
+//       </div>
+//     </figcaption>
+//   </div>
+// </figure>
+
+//     </>
+//   )
+// }
+
+//*******************************************************************************************************************/
+
+// iii } WHEN PROPS VALUE ARE NOT PASSED IN SOME CARD IN APP.JSX
+
+export default function Card({channel,btnText="when btnText is not passed in some card ,then this default value will be used "}) {  //NOTE : using bracket --- {propsName1,propsName2...}
   
   return (
     <>
@@ -66,7 +100,10 @@ export default function Card({channel,btnText}) {  //NOTE : using bracket --- {p
       <div className="text-sky-500 dark:text-sky-400">{channel}
       </div>
       <div className="text-slate-700 dark:text-slate-500">
-         {btnText || "App.jsx didnt pass btnText as props in this card"}       {/* if no value is passed in btnText in card as props then (btnText || "this value will be taken in the place of btnText")  */}
+     {/*    {btnText || "App.jsx didnt pass btnText as props in this card"}        if no value is passed in btnText in card as props then (btnText || "this value will be taken in the place of btnText")  */}
+    
+    {btnText}
+    
       </div>
     </figcaption>
   </div>
@@ -75,4 +112,3 @@ export default function Card({channel,btnText}) {  //NOTE : using bracket --- {p
     </>
   )
 }
-
